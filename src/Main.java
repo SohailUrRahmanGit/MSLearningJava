c31f61dbae24524b93224f35b59bf4de68ca438d
Sohail Ur Rahman S <ssohailmails@gmail.com>
2024-05-02 22:41:02 +0530

    Inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
+import javax.swing.plaf.synth.SynthTextAreaUI;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        
+        System.out.println("Hello World");
+        System.out.println("Hello World Two");
+        Dog dogJack = new Dog();
+        dogJack.setName("Rango");
+        dogJack.setColor("Black");
+        dogJack.setHeight(10.0);
+
+        System.out.println(dogJack.getColor());
     }
 }
